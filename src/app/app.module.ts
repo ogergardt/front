53094cba2cdee27dad1c766a0ad6a67aa3593800
ogergardt@hotmail.com <ogergardt@hotmail.com>
2017-09-12 15:16:28 -0700

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule} from '@angular/material';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthService } from './services/auth.service';
+import { AlertService } from './services/alert.service';
+import { AppRouting } from './app.routing';
+import { IconComponent } from './component/buttons/icon/icon.component';
+import { SearchInputComponent } from './component/search-input/search-input.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    IconComponent,
+    SearchInputComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FlexLayoutModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    AppRouting
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    AlertService
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
