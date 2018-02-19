f0570494f38df173e3a618fa5ffa7bb309f86092
ogergardt@hotmail.com <ogergardt@hotmail.com>
2018-02-18 19:36:51 -0800

    added matGrid with pictures to the Home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// flex-layout
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ObservableMedia } from '@angular/flex-layout';
+
+// material
+import { MatGridListModule } from '@angular/material';
+
+// rxjs
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +24,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public cols: Observable<number>;
+
+  constructor(private observableMedia: ObservableMedia) {}
 
   ngOnInit() {
+    const grid = new Map([
+      ['xs', 1],
+      ['sm', 2],
+      ['md', 2],
+      ['lg', 3],
+      ['xl', 3]
+    ]);
+    let start: number;
+    grid.forEach((cols, mqAlias) => {
+      if (this.observableMedia.isActive(mqAlias)) {
+        start = cols;
+      }
+    });
+    this.cols = this.observableMedia.asObservable()
+      .map(change => grid.get(change.mqAlias))
+      .startWith(start);
   }
-
 }
